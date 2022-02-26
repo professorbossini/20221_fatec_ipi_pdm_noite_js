fd09ededb679a6f13db457839c456d6ba757d372
Rodrigo Bossini <professorbossini@gmail.com>
2022-02-25 21:35:06 -0300

    Retomar da secao 1.5 da apostila 02 falando sobre vetores

--- aula.js
+++ aula.js
@@ -1,1 +1,70 @@
+
+// console.log ('b' + 'a' + + 'a' + 'a')
+// const nome = "a"
+// console.log (nome !== "")
+// //== ===
+// const a = []
+// const b = a
+// console.log (a == b)
+// console.log([] == [])
+// console.log ([] == false)
+// console.log (null == undefined)
+// console.log (null == null)
+// console.log (1 == [1])
+// console.log (true == 1)
+
+
+// console.log (1 == "1")
+// console.log (1 === "1")
+
+
+
+
+
+// console.log ((0.1 + 0.2) === 0.3)
+// console.log(0.1 + 0.2)
+
+
+// const n1 = 2
+// const n2 = '3'
+
+// const n3 = n1 + n2
+// console.log(n3)
+
+// // const n4 = n1 + Number(n2)
+// const n4 = n1 + +n2
+// console.log (n4)
+
+
+// let a = 2
+// a = "abc"
+// var idade = 18
+// console.log ("Oi, " + nome)
+// if (idade >= 18){
+//     //içamento hoist
+//     var nome = "João"
+//     // console.log ("Parabéns, " + nome + ". Você pode dirigir.")
+//     // printf ("Parabéns, %s", nome);
+//     console.log (`Parabéns, ${nome}. Você pode dirigir.`)
+// }
+// console.log ("Até mais, " + nome)
+
+// var linguagem = "Javascript"
+// console.log ("Aprendendo, " + linguagem)
+// linguagem = "Java"
+// console.log ("Aprendendo, " + linguagem)
+
+
 //começando...
+// const nome = "José"
+// const idade = 27
+// nome = "Maria"
+// const a
+
+// let a = 2
+// let b = 'abc'
+// let endereco = `Rua K, 12`
+
+// var c = 2 + 3
+// var d = "abcd"
+
