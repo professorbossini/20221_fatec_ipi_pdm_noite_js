e28d95ff09f7f77e6b5695edb3f517825cb2b8b7
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-11 20:30:20 -0300

    Encerramos o bloco de codigo 3.3.3

--- aula.js
+++ aula.js
@@ -1,16 +1,105 @@
-let p = {
-    nome: "Maria",
-    idade: 22,
-    endereco: {
-        logradouro: "Rua B",
-        numero: 44,
-        bairro: "Vila K"
-    }
-}
-console.log(p.endereco.logradouro)
-console.log(p['endereco'].numero)
-console.log(p.endereco['bairro'])
-console.log(p['endereco']['bairro'])
+//a mesma coisa que a calculoRapidinho faz
+//sem usar "Promise.resolve"
+// const f = (n) => {
+//     return new Promise ((resolve, reject) => {
+//         resolve ((n * (n + 1)) / 2)
+//     })
+// }
+
+// const calculoRapidinho = (n) => {
+//     return Promise.resolve((n * (n + 1)) / 2)
+// }
+// calculoRapidinho(10).then((res) => console.log (res))
+
+// //1 + 2 + ... + n
+// const calculoDemorado = (n) => {
+//     let p = new Promise ((resolve, reject) => {
+//         if (n < 0)
+//             reject ('Não use números negativos')
+//         else{
+//             let res = 0
+//             for (let i = 1; i <= n; i++)
+//                 res += i
+//            resolve(res)
+//         }
+//     })
+//     return p
+// }
+
+// calculoDemorado(-10)
+// .then ((res) => {console.log (res)})
+// .catch((erro) => {console.log ("Erro: " + erro)})
+
+// const r = calculoDemorado(10)
+// r.then()
+
+
+
+
+
+
+// function devolveUm (){
+//     let p = new Promise((resolve, reject) =>{
+//         resolve(1)
+//     })
+//     return p
+// }
+// const resultado = devolveUm()
+// resultado.then((res) => console.log(res))
+
+
+// function devolveUm (){
+//     return 1
+// }
+
+// const resultado = devolveUm()
+// console.log (resultado)
+
+
+
+
+//callback hell
+// const fs = require ('fs')
+// const abrirArquivo = function (nomeArquivo){
+//     const exibirConteudo = function (erro, conteudo){
+//         if (erro){
+//             console.log ('Erro: ' + erro)
+//         }
+//         else{
+//             console.log ("OK: " + conteudo.toString())
+//             const dobro = +conteudo.toString() * 2
+//             console.log ("Valor calculado: " + dobro)
+//             const finalizar = function (erro){
+//                 if (erro)
+//                     console.log ("Erro: " + erro)
+//                 else
+//                     console.log ("OK, escreveu")
+//             }
+//             fs.writeFile('dobro.txt', dobro.toString(), finalizar)
+//         }
+//     }
+//     //segundo parâmetro: função callback
+//     fs.readFile('arquivo.txt', exibirConteudo);
+// }
+
+// abrirArquivo('arquivo.txt')
+
+//IO-Bound
+//CPU-Bound
+
+// let p = {
+//     nome: "Maria",
+//     idade: 22,
+//     endereco: {
+//         logradouro: "Rua B",
+//         numero: 44,
+//         bairro: "Vila K"
+//     }
+// }
+// console.log(p.endereco.logradouro)
+// console.log(p['endereco'].numero)
+// console.log(p.endereco['bairro'])
+// console.log(p['endereco']['bairro'])
 // let pessoa = {
 //     nome: "João",
 //     idade: 17
